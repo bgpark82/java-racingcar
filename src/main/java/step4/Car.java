1a65e924eba2d991cbe5236174dfd92c65d1382e
bgpark <bgpark@citus.co.kr>
2020-06-08 16:35:41 +0900

    feat : Car 구현 및 Car의 팩토리 메소드 생성

--- src/main/java/step4/Car.java
+++ src/main/java/step4/Car.java
@@ -5,21 +5,50 @@ import java.util.Objects;
 public class Car {
 
     private final String name;
+    private int position;
 
-    public Car(String name) {
-        this.name = name;
+    public static Car of(String name) {
+        return new Car(name, 0);
     }
+
+    Car(String name, int position) {
+        invalid(name);
+        this.name = name;
+        this.position = position;
+    }
+
+
+    private void invalid(String name) {
+        if(name == "") {
+            throw new IllegalArgumentException();
+        }
+        if(name == null) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void move(int fuel) {
+        if(fuel >= 4) {
+            this.position += 1;
+        }
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Car car = (Car) o;
-        return name.equals(car.name);
+        return position == car.position &&
+                Objects.equals(name, car.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(name, position);
     }
 }
--- src/test/java/step4/CarTest.java
+++ src/test/java/step4/CarTest.java
@@ -2,13 +2,31 @@ package step4;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class CarTest {
 
     @Test
     void create() {
-        Car car = new Car("peter");
-        assertThat(car).isEqualTo(new Car("peter"));
+        Car car = Car.of("peter");
+        assertThat(car).isEqualTo(Car.of("peter"));
+    }
+
+    @Test
+    void name_is_not_null_or_empty() {
+        assertThatThrownBy(() -> {
+            Car.of("");
+        }).isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> {
+            Car.of(null);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void move() {
+        Car peter = Car.of("peter");
+        peter.move(4);
+        assertThat(peter.getPosition()).isEqualTo(1);
     }
 }
